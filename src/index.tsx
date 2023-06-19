4c0f829cd899fe014de2d831e04bb6b2e8468a46
cho sang joon <sjbio234@naver.com>
2023-06-19 16:19:16 +0900

    env and key

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase/compat/app";
+
+console.log(firebase);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
